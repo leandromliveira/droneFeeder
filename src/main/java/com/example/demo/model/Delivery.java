4487ea317230ba22d4d5fb138a9674f905920b62
Leandro <leandrooliveira35@gmail.com>
2022-07-11 11:37:14 -0300

    feat: create models drone and delivery

---
+++ src/main/java/com/example/demo/model/Delivery.java
@@ -0,0 +1,93 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Delivery {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @ManyToOne
+  @JoinColumn(name = "drone_id")
+  private Drone drone;
+
+  private String longitude;
+  private String latitude;
+  private LocalDateTime postedDate;
+  private LocalDateTime deliveredDate;
+  private String deliveryStatys;
+  private String video;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public LocalDateTime getPostedDate() {
+    return postedDate;
+  }
+
+  public void setPostedDate(LocalDateTime postedDate) {
+    this.postedDate = postedDate;
+  }
+
+  public LocalDateTime getDeliveredDate() {
+    return deliveredDate;
+  }
+
+  public void setDeliveredDate(LocalDateTime deliveredDate) {
+    this.deliveredDate = deliveredDate;
+  }
+
+  public String getDeliveryStatys() {
+    return deliveryStatys;
+  }
+
+  public void setDeliveryStatys(String deliveryStatys) {
+    this.deliveryStatys = deliveryStatys;
+  }
+
+  public String getVideo() {
+    return video;
+  }
+
+  public void setVideo(String video) {
+    this.video = video;
+  }
+
+  public Drone getDrone() {
+    return drone;
+  }
+
+  public void setDrone(Drone drone) {
+    this.drone = drone;
+  }
+
+}
---
+++ src/main/java/com/example/demo/model/Drone.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Drone {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @OneToMany(mappedBy = "drone", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Delivery> deliveries;
+  private String latitude;
+  private String longitude;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public List<Delivery> getDeliveries() {
+    return deliveries;
+  }
+
+  public void setDeliveries(List<Delivery> deliveries) {
+    this.deliveries = deliveries;
+  }
+
+  public void addDelivery(Delivery delivery) {
+    this.deliveries.add(delivery);
+  }
+
+}
