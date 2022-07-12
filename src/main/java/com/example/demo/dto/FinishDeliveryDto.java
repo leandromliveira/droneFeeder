f7ec2b5f503d4746042b9c3251336f923bdd029f
Leandro <leandrooliveira35@gmail.com>
2022-07-12 16:41:45 -0300

    feat: create FinishDeliveryDto

---
+++ src/main/java/com/example/demo/dto/FinishDeliveryDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+public class FinishDeliveryDto {
+  private String video;
+
+  public String getVideo() {
+    return video;
+  }
+
+  public void setVideo(String video) {
+    this.video = video;
+  }
+
+}
