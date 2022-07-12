99684e789429ddd2a1531e295b35b609c5ca2426
Leandro <leandrooliveira35@gmail.com>
2022-07-12 10:46:58 -0300

    feat: create DroneService

---
+++ src/main/java/com/example/demo/service/DroneService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Drone;
+import com.example.demo.repository.DroneRepository;
+
+@Service
+public class DroneService {
+  @Autowired
+  private DroneRepository repository;
+
+  public Drone create() {
+    Drone newDrone = new Drone();
+    newDrone.setLatitude("-15.9041343");
+    newDrone.setLongitude("-48.1299912");
+    return repository.save(newDrone);
+  }
+
+  public List<Drone> getAllDrones() {
+    return repository.findAll();
+  }
+
+  public Drone getDroneById(int id) {
+    return repository.findById(id).orElse(null);
+  }
+
+  public void delete(int id) {
+    repository.deleteById(id);
+  }
+
+}
