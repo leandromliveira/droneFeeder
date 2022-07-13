ba02cec984b8242509283438ab0d750d5555a55b
Leandro <leandrooliveira35@gmail.com>
2022-07-13 11:50:19 -0300

    feat: TesteRepository learn to save video flies

---
+++ src/main/java/com/example/demo/repository/TesteRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Teste;
+
+@Repository
+public interface TesteRepository extends JpaRepository<Teste, Integer> {
+}
