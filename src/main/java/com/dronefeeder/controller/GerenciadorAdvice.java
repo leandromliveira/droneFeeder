9487c4020f83dc642b4aacd65b0490776fb2c3ae
leandromliveira <leandrooliveira35@gmail.com>
2022-07-18 21:12:01 -0300

    feat: add dronenotfoundexception and videoNotfoundException in gerenciador advice

--- src/main/java/com/dronefeeder/controller/GerenciadorAdvice.java
+++ src/main/java/com/dronefeeder/controller/GerenciadorAdvice.java
@@ -3,6 +3,9 @@ package com.dronefeeder.controller;
 import com.dronefeeder.exception.DataError;
 import com.dronefeeder.exception.DeliveryNotFoundException;
 import com.dronefeeder.exception.DroneNotAvailableException;
+import com.dronefeeder.exception.DroneNotFoundException;
+import com.dronefeeder.exception.VideoNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +14,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GerenciadorAdvice {
-  @ExceptionHandler({DeliveryNotFoundException.class})
+  @ExceptionHandler({DeliveryNotFoundException.class, 
+      DroneNotFoundException.class, VideoNotFoundException.class})
   public ResponseEntity<DataError> handlerNotFound(RuntimeException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
       .body(new DataError(exception.getMessage()));
