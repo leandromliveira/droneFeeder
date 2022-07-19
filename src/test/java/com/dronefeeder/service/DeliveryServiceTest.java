2ee8eb6eae48b4a9bdf3ee5c1ac88b46d440b27b
leandromliveira <leandrooliveira35@gmail.com>
2022-07-18 21:07:17 -0300

    feat: create test to catch delivery not found exception

--- src/test/java/com/dronefeeder/service/DeliveryServiceTest.java
+++ src/test/java/com/dronefeeder/service/DeliveryServiceTest.java
@@ -3,6 +3,8 @@ package com.dronefeeder.service;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.dronefeeder.exception.DeliveryNotFoundException;
+import com.dronefeeder.exception.DroneNotAvailableException;
 import com.dronefeeder.model.Delivery;
 import com.dronefeeder.model.Drone;
 import com.dronefeeder.model.Video;
@@ -10,6 +12,7 @@ import com.dronefeeder.repository.DeliveryRepository;
 import com.dronefeeder.repository.DroneRepository;
 import com.dronefeeder.repository.VideoRepository;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -44,11 +47,11 @@ public class DeliveryServiceTest {
   @DisplayName("must be able to create a delivery")
   public void createDelivery() throws Exception {
     Delivery delivery = new Delivery();
-    // List<Delivery> deliveries; 
-    // deliveries = Collections.singletonList(delivery);
+    List<Delivery> deliveries; 
+    deliveries = Collections.singletonList(delivery);
     Drone drone = new Drone();
-    // drone.addDelivery(delivery);
     drone.setAvailable(true);
+    drone.addDelivery(delivery);
     List<Drone> drones = Collections.singletonList(drone);
 
     Mockito.when(droneRepository.findAll()).thenReturn(drones);
@@ -59,7 +62,7 @@ public class DeliveryServiceTest {
     Assertions.assertEquals(delivery, service
         .create(delivery.getLatitude(), delivery.getLongitude()));
 
-    // Assertions.assertEquals(deliveries, droneRepository.findAll().get(0).getDeliveries());
+    Assertions.assertEquals(deliveries, droneRepository.findAll().get(0).getDeliveries());
   }
 
   @Test
@@ -112,4 +115,46 @@ public class DeliveryServiceTest {
     service.finishDelivery(delivery.getId(), video.getId());
   }
 
+  @Test
+  @DisplayName("should throw an exception because it is not possible to create a delivery without a drone available")
+  public void throwExceptionInCreateDelivery() throws Exception {
+    Delivery delivery = new Delivery();
+    Drone drone = new Drone();
+    drone.setAvailable(false);
+    List<Drone> drones = Collections.singletonList(drone);
+
+    Mockito.when(droneRepository.findAll()).thenReturn(drones);
+    
+    Mockito.when(repository.save(Mockito.any(Delivery.class))).thenReturn(delivery);
+
+  
+    Assertions.assertThrows(DroneNotAvailableException.
+        class, () -> service
+        .create(delivery.getLatitude(), delivery.getLongitude()));
+  }
+
+  @Test
+  @DisplayName("should throw an exception because it is not possible to finalize a delivery not found")
+  public void throwExceptionInFinishDelivery() throws Exception {
+    Delivery delivery = new Delivery();
+    Drone drone = new Drone();
+    delivery.setDrone(drone);
+
+    Video video = new Video();
+    Optional<Video> opVideo = Optional.of(video);
+
+    Mockito.when(videoRepository.findById(delivery.getId())).thenReturn(opVideo);
+
+    Assertions.assertThrows(DeliveryNotFoundException.
+        class, () -> service.finishDelivery(0, video.getId()));
+  }
+
+  @Test
+  @DisplayName("should throw an exception if the delivery is not found")
+  public void throwExceptionVideoNotFound() throws IOException {
+    Integer nonExistentId = 404;
+    Mockito.when(repository.findById(nonExistentId)).thenReturn(Optional.empty());
+
+    Assertions.assertThrows(DeliveryNotFoundException.class, () -> service.getDeliveryById(nonExistentId));
+  }
 }
