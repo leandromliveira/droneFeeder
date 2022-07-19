88cf6d0ecea2a0a5a6fb9edbdff1ddf203f53d39
leandromliveira <leandrooliveira35@gmail.com>
2022-07-18 21:11:14 -0300

    feat: create constructor in drone model

--- src/main/java/com/dronefeeder/model/Drone.java
+++ src/main/java/com/dronefeeder/model/Drone.java
@@ -1,6 +1,8 @@
 package com.dronefeeder.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -22,6 +24,10 @@ public class Drone {
   private String latitude;
   private String longitude;
   private Boolean available;
+
+  public Drone() {
+    this.deliveries = new ArrayList<Delivery>();
+  }
 
   public int getId() {
     return id;
